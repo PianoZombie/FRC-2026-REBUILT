c729db7bd50dd5b9807247d841d77da690ccfcae
ured116 <ian.cned@gmail.com>
2026-02-10 17:21:32 -0500

    Fixed sim skeleton

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -24,11 +22,10 @@ import frc.robot.subsystems.DriveSubsystem;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  private final DriveSubsystem drive;
   private Vision vision;
   private final RobotContainer m_robotContainer;
 
-  private final Field2d m_field = new Field2d();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -38,12 +35,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    vision = new Vision(m_robotContainer.getDriveSubsystem());
-  }
-
-  @Override
-  public void robotInit() {
-    SmartDashboard.putData("Field", m_field);
+    drive = new DriveSubsystem();
+    vision = new Vision(drive);
   }
 
   /**
@@ -61,7 +54,6 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
     vision.periodic();
-    m_field.setRobotPose(m_robotContainer.getDriveSubsystem().getPose());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -39,7 +39,6 @@ public class MAXSwerveModule {
    * Encoder.
    */
   public MAXSwerveModule(int drivingCANId, int turningCANId, double chassisAngularOffset) {
-    System.out.println("Creating MAXSwerveModule: Drive ID " + drivingCANId + ", Turn ID " + turningCANId);
     m_drivingSpark = new SparkMax(drivingCANId, MotorType.kBrushless);
     m_turningSpark = new SparkMax(turningCANId, MotorType.kBrushless);
 
