93db1b5d393a7696971d803a7ae21a5d0e5679af
ured116 <ian.cned@gmail.com>
2026-03-02 08:33:33 -0500

    Changed to WPILib PID controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -147,9 +147,6 @@ public final class Constants {
   public static final class ClimberConstants {
     public static final int oneStageCanID = 9;
     public static final int twoStageCanID = 10;
-    public static final double kP = 0.1;
-    public static final double kI = 0.0;
-    public static final double kD = 0.0;
   }
 
   public static final class VisionConstants {
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,22 +5,22 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
   private final SparkMax oneStageMotor;
   private final SparkMax twoStageMotor;
-  private final SparkClosedLoopController oneStageController;
-  private final SparkClosedLoopController twoStageController;
   private final SparkMaxConfig motorConfig;
+  private final PIDController oneStagePID;
+  private final PIDController twoStagePID;
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
@@ -31,26 +31,22 @@ public class ClimberSubsystem extends SubsystemBase {
     motorConfig.idleMode(SparkBaseConfig.IdleMode.kBrake);
     motorConfig.inverted(false);
     motorConfig.smartCurrentLimit(40);
-    /** Configure PID for position control */
-    motorConfig.closedLoop
-      .p(ClimberConstants.kP) // please tune
-      .i(ClimberConstants.kI)
-      .d(ClimberConstants.kD)
-      .outputRange(-1, 1);
 
-      oneStageMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-      twoStageMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    oneStageMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    twoStageMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-      oneStageController = oneStageMotor.getClosedLoopController();
-      twoStageController = twoStageMotor.getClosedLoopController();
+    oneStagePID = new PIDController(0.1, 0, 0);
+    twoStagePID = new PIDController(0.1, 0, 0);
   }
   /**Drive motor to first stage in rotations */
   public void setOneStage(double position) {
-    oneStageController.setSetpoint(position, ControlType.kPosition);
+    double output = oneStagePID.calculate(oneStageMotor.getEncoder().getPosition(), position);
+    oneStageMotor.set(output);
   }
   /**Drive motor to second stage in rotations */
   public void setTwoStage(double position) {
-    twoStageController.setSetpoint(position, ControlType.kPosition);
+    double output = twoStagePID.calculate(twoStageMotor.getEncoder().getPosition(), position);
+    twoStageMotor.set(output);
   }
 
   @Override
